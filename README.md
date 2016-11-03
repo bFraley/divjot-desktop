bc8806e1484254dae84bfdae8c8f79e938ac3bff
bFraley <bFraley@users.noreply.github.com>
2016-11-03 01:46:22 -0400

    a basic implementation of divjot to kick of UI dev

---
+++ README.md
@@ -0,0 +1,5 @@
+# Divjot
+
+This is the development repository for the cross platform desktop version of Divjot, a simple and powerful live web editor tool.
+
+The TryDivjot organization was made around developing and shipping this new desktop version of Divjot. There will be a website, releases on the vendor platform stores, and a range of APIs and plugin features coming soon.
--- divjot.js
+++ divjot.js
@@ -2,12 +2,27 @@
 // Copyright 2016
 
 (function() {
-  let main_output = document.getElementById('main-output');
-  let main_editor = document.getElementById('main-editor');
 
+  /* Markup and Style output elements. */
+  let main_output = document.getElementById('main-output');
+  let style_output = document.getElementById('style-output');
+
+  /* Editor elements. */
+  let main_editor = document.getElementById('main-editor');
+  let style_editor = document.getElementById('style-editor');
+  let js_editor = document.getElementById('js-editor');
+
+  /* Editor event listeners */
   main_editor.addEventListener('keyup', function() {
       main_output.innerHTML = main_editor.value;
   }, false);
 
+  style_editor.addEventListener('keyup', function() {
+      style_output.innerHTML = style_editor.value;
+  }, false);
+
+  js_editor.addEventListener('keyup', function() {
+      return eval(js_editor.value)
+  }, false);
 
 }());
